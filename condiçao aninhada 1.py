33318626e65f1c648a5b6179f58159bc9fe56069
Denys Dos Santos Mota <hakermota@gmail.com>
2022-06-18 00:27:30 -0300

    Add files via upload
    add de novo exercícios função ellse if e elise com uma calculadora de escolha com conversão para hexadecimal octal e binaria

---
+++ condiçao aninhada 1.py
@@ -0,0 +1,15 @@
+print('\033[31m     CALCULO DE CONVERÇÃO BINARIA\033[m')
+numero = int(input('Porfavo digite o numero para converção? = '))
+escolha = int(input('1-Para binário\n2-Para octal\n3-Para hexadecimal\nSua escolha e = '))
+binário = bin(numero)
+octal = oct(numero)
+hexadecimal = hex(numero)
+if escolha == 1 :
+    print('Sua escolha foi binário')
+    print('O valor para binário e = {}'.format(binário))
+elif escolha == 2 :
+    print('Sua escolha foi octal')
+    print('O valor para octal e = {}'.format(octal))
+elif escolha == 3 :
+    print('Sua escolha foi hexadecimal')
+    print('O valor para hexadecimal e = {}'.format(hexadecimal))
